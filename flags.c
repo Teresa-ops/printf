62f8cad222072c68773543c18d1e9dd3b73a095f
Margaret Mwangi <129050745+Margaret300@users.noreply.github.com>
2023-05-27 13:10:34 +0300

    Update flags.c

--- flags.c
+++ flags.c
@@ -2,7 +2,7 @@
 
 /**
  * get_flags - Calculates active flags
- * by names
+ * by Margaret and Teresa
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
  * Return: Flags:
