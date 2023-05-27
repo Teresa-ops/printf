02551045a050735969a7f8454af00badb2f26ca5
Teresa-ops <trsmwaura@gmail.com>
2023-05-27 05:17:41 -0700

    precision

--- precision.c
+++ precision.c
@@ -2,7 +2,7 @@
 
 /**
  * get_precision - Calculates the precision for printing
- * by Margaret and Teresa
+ * by margaret and teresa
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
